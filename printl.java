1c2a4fef617a0b6b8a2b8a7fd3e2e86865d2b630
Gopinath <133114658+GopinathcKing@users.noreply.github.com>
2023-08-30 09:17:04 +0530

    Print "L" with asterisk symbol

---
+++ printl.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class printl {
+    public void printnow(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your J's size here : ");
+        int user = in.nextInt();
+        for(int i=1;i<=user; i++){
+            for(int j=1;j<=user;j++){
+                if(j==1||i==user)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
